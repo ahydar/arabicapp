4867fa94c3aab43af6678efa6d82cae477e2784e
dallah20 <3140171@myuwc.ac.za>
2017-05-16 17:08:47 +0200

    Add Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="/css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <example></example>
+
+            <test></test>
+
+            <big></big>
+        </div>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
